ea0f2f089c0b1f321c604bf326afb146534eefce
Vyacheslav Pechenov <84916321+SLEMSIK@users.noreply.github.com>
2023-01-26 01:17:20 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.get('/his7', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
     app.use(express.static(__dirname + '/public'));
 })
-
+app.get('/his7/teacher1234', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'admin-tool', 'index.html'))
+  app.use(express.static(__dirname + '/admin-tool'));
+})
 app.get('/', (req, res) =>{
   res.send("<h1>Write test adress</h1>")
 })
@@ -20,7 +23,7 @@ app.post('/his7/result/:name/:ball/:mark', (req, res) => {
     const ball = req.params.ball;
     const mark = req.params.mark
     res.send(`${name}:${ball}:${mark}`)
-    fs.appendFile('student.csv', `${name};${ball};${mark}\n`, 'utf8', function (err) {
+    fs.appendFile('admin-tool/student.csv', `${name},${ball},${mark}\n`, 'utf8', function (err) {
         if (err) {
           console.log("Error");
         } else{
