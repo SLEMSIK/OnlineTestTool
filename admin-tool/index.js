ddb7c25457504ba244b71880f360eb0112ff8f4a
Vyacheslav Pechenov <84916321+SLEMSIK@users.noreply.github.com>
2023-01-26 01:16:50 +0300

    Add files via upload

---
+++ admin-tool/index.js
@@ -0,0 +1,39 @@
+function successFunction(data) {
+    var allRows = data.split(/\r?\n|\r/);
+    var table = '<table>';
+    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+      if (singleRow === 0) {
+        table += '<thead>';
+        table += '<tr>';
+      } else {
+        table += '<tr>';
+      }
+      var rowCells = allRows[singleRow].split(',');
+      for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+        if (singleRow === 0) {
+          table += '<th>';
+          table += rowCells[rowCell];
+          table += '</th>';
+        } else {
+          table += '<td>';
+          table += rowCells[rowCell];
+          table += '</td>';
+        }
+      }
+      if (singleRow === 0) {
+        table += '</tr>';
+        table += '</thead>';
+        table += '<tbody>';
+      } else {
+        table += '</tr>';
+      }
+    } 
+    table += '</tbody>';
+    table += '</table>';
+    $('body').append(table);
+  }
+
+$.ajax({
+    url: '/student.csv',
+    dataType: 'text',
+  }).done(successFunction);
